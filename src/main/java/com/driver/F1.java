0ece0d39458245708db459fdb9f858f1fbbd5401
Kartik Keshav Gaikwad <kartikgaikwad2004@gmail.com>
2025-12-20 21:30:30 +0530

    Update F1.java

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -1,30 +1,23 @@
 package com.driver;
 
-public class F1 extends Car {
+public class F1 implements Vehicle {
 
-    public F1(String name, boolean isManual) {
-        //Use arbitrary values for parameters which are not mentioned
+    private String name;
+    private int capacity;
+
+    public F1(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
     }
 
-    public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
-        /**
-         * speed 0: gear 1
-         * speed 1-50: gear 1
-         * speed 51-100: gear 2
-         * speed 101-150: gear 3
-         * speed 151-200: gear 4
-         * speed 201-250: gear 5
-         * speed more than 250: gear 6
-         */
+    @Override
+    public String getVehicleName() {
+        return name;
+    }
 
-        if(newSpeed == 0) {
-            //Stop the car, set gear as 1
-        }
-        //for all other cases, change the gear accordingly
-
-        if(newSpeed > 0) {
-            changeSpeed(newSpeed, getCurrentDirection());
-        }
+    @Override
+    public int getVehicleCapacity() {
+        return capacity;
     }
 }
+
