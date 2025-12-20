d92bfe6eeb3c2e536eb6a3a72128c9bc71672326
Kartik Keshav Gaikwad <kartikgaikwad2004@gmail.com>
2025-12-20 22:03:27 +0530

    Update Car.java

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -1,62 +1,23 @@
 package com.driver;
 
-public class Car extends Vehicle {
-    private int wheels;
-    private String type;
-    private int doors;
-    private int gears;
-    private boolean isManual;
-    private int currentGear;
-    private int seats;
+public class Car implements Vehicle {
 
-    public int getWheels() {
-        return wheels;
+    private String name;
+    private int capacity;
+
+    public Car(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
     }
 
-    public String getType() {
-        return type;
+    @Override
+    public String getVehicleName() {
+        return name;
     }
 
-    public int getGears() {
-        return gears;
-    }
-
-    public int getDoors() {
-        return doors;
-    }
-
-    public boolean isManual() {
-        return isManual;
-    }
-
-    public int getCurrentGear() {
-        return currentGear;
-    }
-
-    public int getSeats() {
-        return seats;
-    }
-
-    public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
-        //Hint: Car extends Vehicle
-        super(name);
-        this.wheels = wheels;
-        this.type = type;
-        this.doors = doors;
-        this.gears = gears;
-        this.isManual = isManual;
-        this.seats = seats;
-        this.currentGear = 1;
-    }
-
-    public void changeGear(int newGear) {
-        currentGear = newGear;
-        System.out.println("changeGear method called - The gear is changed to: " + currentGear);
-    }
-
-    public void changeSpeed(int newSpeed, int newDirection) {
-        setCurrentSpeed(newSpeed);
-        setCurrentDirection(newDirection);
-        System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
+    @Override
+    public int getVehicleCapacity() {
+        return capacity;
     }
 }
+
