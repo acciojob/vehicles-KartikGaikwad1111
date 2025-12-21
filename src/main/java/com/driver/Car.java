74176e6891d88116d4970e0ed8bf4f7f93eb601d
Kartik Keshav Gaikwad <kartikgaikwad2004@gmail.com>
2025-12-21 09:41:26 +0530

    Update Car.java

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package com.driver;
 
 public class Car extends Vehicle {
@@ -12,48 +7,56 @@ public class Car extends Vehicle {
     private int gears;
     private boolean isManual;
     private int currentGear;
+    private int seats;
 
-    public Car(String name, int wheels, int doors, int gears, boolean isManual, String type) {
+    public int getWheels() {
+        return wheels;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getGears() {
+        return gears;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isManual() {
+        return isManual;
+    }
+
+    public int getCurrentGear() {
+        return currentGear;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
+        //Hint: Car extends Vehicle
         super(name);
         this.wheels = wheels;
+        this.type = type;
         this.doors = doors;
         this.gears = gears;
         this.isManual = isManual;
-        this.type = type;
+        this.seats = seats;
         this.currentGear = 1;
     }
 
     public void changeGear(int newGear) {
-        this.currentGear = newGear;
-        System.out.println("changeGear method called - The gear is changed to: " + this.currentGear);
+        currentGear = newGear;
+        System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection) {
-        this.move(newSpeed, newDirection);
+        setCurrentSpeed(newSpeed);
+        setCurrentDirection(newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
-    }
-
-    public int getWheels() {
-        return this.wheels;
-    }
-
-    public String getType() {
-        return this.type;
-    }
-
-    public int getDoors() {
-        return this.doors;
-    }
-
-    public int getGears() {
-        return this.gears;
-    }
-
-    public boolean isManual() {
-        return this.isManual;
-    }
-
-    public int getCurrentGear() {
-        return this.currentGear;
     }
 }
