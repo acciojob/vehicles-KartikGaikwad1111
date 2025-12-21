0eb05e471e327a6869ada2366747659be28fa5cc
Kartik Keshav Gaikwad <kartikgaikwad2004@gmail.com>
2025-12-21 09:11:30 +0530

    Update Vehicle.java

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -1,20 +1,59 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package com.driver;
 
-public class Vehicle {
+public class Car extends Vehicle {
+    private int wheels;
+    private String type;
+    private int doors;
+    private int gears;
+    private boolean isManual;
+    private int currentGear;
 
-    String name;
-    int capacity;
-
-    public Vehicle(String name, int capacity) {
-        this.name = name;
-        this.capacity = capacity;
+    public Car(String name, int wheels, int doors, int gears, boolean isManual, String type) {
+        super(name);
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.type = type;
+        this.currentGear = 1;
     }
 
-    public String getVehicleName() {
-        return name;
+    public void changeGear(int newGear) {
+        this.currentGear = newGear;
+        System.out.println("changeGear method called - The gear is changed to: " + this.currentGear);
     }
 
-    public int getVehicleCapacity() {
-        return capacity;
+    public void changeSpeed(int newSpeed, int newDirection) {
+        this.move(newSpeed, newDirection);
+        System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
+    }
+
+    public int getWheels() {
+        return this.wheels;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public int getDoors() {
+        return this.doors;
+    }
+
+    public int getGears() {
+        return this.gears;
+    }
+
+    public boolean isManual() {
+        return this.isManual;
+    }
+
+    public int getCurrentGear() {
+        return this.currentGear;
     }
 }
