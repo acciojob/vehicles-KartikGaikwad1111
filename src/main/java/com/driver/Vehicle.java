185fc9ba66ad094338919e7b7601f5e09d8aaaa4
Kartik Keshav Gaikwad <kartikgaikwad2004@gmail.com>
2025-12-21 09:06:26 +0530

    Update Vehicle.java

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -1,6 +1,20 @@
 package com.driver;
 
-public interface Vehicle {
-    String getVehicleName();
-    int getVehicleCapacity();
+public class Vehicle {
+
+    String name;
+    int capacity;
+
+    public Vehicle(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    public String getVehicleName() {
+        return name;
+    }
+
+    public int getVehicleCapacity() {
+        return capacity;
+    }
 }
