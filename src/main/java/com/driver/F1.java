99364990793d622232207b27c95a4d359248400c
Kartik Keshav Gaikwad <kartikgaikwad2004@gmail.com>
2025-12-21 09:09:19 +0530

    Update F1.java

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -1,23 +1,33 @@
+
 package com.driver;
 
-public class F1 implements Vehicle {
-
-    private String name;
-    private int capacity;
-
-    public F1(String name, int capacity) {
-        this.name = name;
-        this.capacity = capacity;
+public class F1 extends Car {
+    public F1(String name, boolean isManual) {
+        super(name, 4, 2, 6, isManual, "F1");
     }
 
-    @Override
-    public String getVehicleName() {
-        return name;
-    }
+    public void accelerate(int rate) {
+        int newSpeed = this.getCurrentSpeed() + rate;
+        if (newSpeed == 0) {
+            this.stop();
+            this.changeGear(1);
+        } else if (newSpeed > 0 && newSpeed <= 50) {
+            this.changeGear(1);
+        } else if (newSpeed > 50 && newSpeed <= 100) {
+            this.changeGear(2);
+        } else if (newSpeed > 100 && newSpeed <= 150) {
+            this.changeGear(3);
+        } else if (newSpeed > 150 && newSpeed <= 200) {
+            this.changeGear(4);
+        } else if (newSpeed > 200 && newSpeed <= 250) {
+            this.changeGear(5);
+        } else {
+            this.changeGear(6);
+        }
 
-    @Override
-    public int getVehicleCapacity() {
-        return capacity;
+        if (newSpeed > 0) {
+            this.changeSpeed(newSpeed, this.getCurrentDirection());
+        }
+
     }
 }
-
