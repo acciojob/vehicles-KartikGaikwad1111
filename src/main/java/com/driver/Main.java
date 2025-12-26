4e4607f040f5a5adc0f365d3603607c985e5207b
Kartik Keshav Gaikwad <kartikgaikwad2004@gmail.com>
2025-12-26 17:45:38 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,10 +1,23 @@
-package com.driver;
-
 public class Main {
     public static void main(String[] args) {
-        F1 mercedes = new F1("mercedes", false);
-        mercedes.move(40, 0);
-        mercedes.accelerate(20);
-        mercedes.accelerate(-60);
+        // Create instances of Vehicle, Car, and F1
+        Vehicle vehicle = new Vehicle("Generic Vehicle", 4);
+        Car car = new Car("Sedan", 5);
+        F1 f1 = new F1("Formula 1", 2);
+        
+        // Create an instance of Boat
+        Boat boat = new Boat("Speedboat", 6);
+
+        // Testing the Vehicle class
+        System.out.println("Vehicle Name: " + vehicle.getVehicleName() + ", Capacity: " + vehicle.getVehicleCapacity());
+        
+        // Testing the Car class
+        System.out.println("Car Name: " + car.getVehicleName() + ", Capacity: " + car.getVehicleCapacity());
+        
+        // Testing the F1 class
+        System.out.println("F1 Name: " + f1.getVehicleName() + ", Capacity: " + f1.getVehicleCapacity());
+        
+        // Testing the Boat class
+        System.out.println("Boat Name: " + boat.getVehicleName() + ", Capacity: " + boat.getVehicleCapacity());
     }
 }
