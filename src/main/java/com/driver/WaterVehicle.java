41579b19481c7c9818c4e32ba355c83e58d3f0e7
Kartik Keshav Gaikwad <kartikgaikwad2004@gmail.com>
2025-12-26 17:59:19 +0530

    Update WaterVehicle.java

--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -1,3 +1,4 @@
+// Step 4: Define the WaterVehicle interface
 interface WaterVehicle {
     String getVehicleName(); // Method to get the name of the water vehicle
     int getVehicleCapacity(); // Method to get the capacity of the water vehicle
