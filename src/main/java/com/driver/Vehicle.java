db69048f2f52a1aea77e2f9ac38f372729ea21ff
Kartik Keshav Gaikwad <kartikgaikwad2004@gmail.com>
2025-12-26 17:51:21 +0530

    Update Vehicle.java

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -19,3 +19,10 @@ class Vehicle {
         return capacity;
     }
 }
+// Step 2: Define the Car class that extends Vehicle
+class Car extends Vehicle {
+    // Constructor
+    public Car(String name, int capacity) {
+        super(name, capacity); // Call the constructor of the Vehicle class
+    }
+}
